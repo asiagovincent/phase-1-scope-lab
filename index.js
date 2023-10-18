3807fdab813e64da928796362c2e9bcba800f7ea
asiagovincent11@gmail.com <asiagovincent11@gmail.com>
2023-10-18 21:21:01 +0300

    done with the lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'someone else';
+}
+
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer(); // This will throw an error
+
+console.log(customerName); // Output: BOB
+console.log(bestCustomer); // Output: maybe bob
+console.log(leastFavoriteCustomer); // Output: someone
+
